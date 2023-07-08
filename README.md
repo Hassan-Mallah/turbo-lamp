a036f5576e79be27132f755787e2aa0af70c93f0
Hassan <hassansmallah@gmail.com>
2023-07-08 11:36:08 +0200

    create proejct

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # turbo-lamp
-RabbitMQ + Python
+RabbitMQ + Python: Creates task producer that works with several consumers
+
+### RabbitMQ
+
+Run from docker:
+
+    # version 3.12
+    docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
--- producer/producer.py
+++ producer/producer.py
@@ -1,1 +1,24 @@
 #  The main Python script for the producer
+import pika
+import json
+
+# establish a connection to RabbitMQ
+host = 'localhost'
+connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+channel = connection.channel()
+
+# declare the queue, durable to save queue when restart
+channel.queue_declare(queue='task_queue', durable=True)
+
+# prepare the task data
+task = {
+    'id': 1,
+    'name': 'Task 1',
+    'data': {
+        'param1': 'value1',
+        'param2': 'value2'
+    }
+}
+
+# Convert the task data to JSON format
+task_json = json.dumps(task)
